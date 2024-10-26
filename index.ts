656add23d1314a48e064a63f297c5919805b3433
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-10-26 18:35:05 +0200

    remove project files

--- index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./server/app";
-
-const server = Bun.serve({
-  fetch: app.fetch,
-});
-
-if (process.env.NODE_ENV === "development")
-  console.log("Listening on http://127.0.0.1:3000/api");
