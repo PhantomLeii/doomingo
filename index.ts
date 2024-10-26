3a2410da326b3c128e7c9edfba8fc671947eac4a
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-10-26 12:59:26 +0200

    Server setup (#2)
    * Setup: add served hono app
    
    * setup: add server setup files

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import app from "./server/server";
+
+export const server = Bun.serve({
+  hostname: "0.0.0.0",
+  fetch: app.fetch,
+});
+
+console.log(`Listening on ${process.env.BASE_URL}`);
